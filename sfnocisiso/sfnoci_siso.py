ec7553a91759bf9c9a12e03c53cf4219f8cd1cfc
jiseong_QClab <fark4308@snu.ac.kr>
2025-08-26 18:51:07 +0900

    Update

---
+++ sfnocisiso/sfnoci_siso.py
@@ -0,0 +1,304 @@
+import os
+import numpy
+import scipy.linalg
+from pyscf.sfnoci import sfnoci
+from pyscf import fci
+from pyscf.fci import selected_ci
+from pyscf.data import nist
+from pyscf import lib
+from fcisiso import SU2CG
+from fcisiso import get_jk, get_jk_amfi, compute_hso_ao
+import pySFNOCI.selected_SFNOCI_new as selected_SFNOCI
+import copy
+
+def make_trans(m, cibra, ciket, norb, nelec_bra, nelec_ket,po_list,group,ov_list):
+    if m == 1:
+        return -1.0 * make_trans_rdm1('ab', cibra, ciket, norb, nelec_bra, nelec_ket, po_list, group, ov_list)
+    elif m == -1:
+        return make_trans_rdm1('ba', cibra, ciket, norb, nelec_bra, nelec_ket, po_list, group, ov_list)
+    else:
+        return numpy.sqrt(0.5) * (make_trans_rdm1('aa', cibra, ciket, norb, nelec_bra, nelec_ket, po_list, group, ov_list)
+                               - make_trans_rdm1('bb', cibra, ciket, norb, nelec_bra, nelec_ket, po_list, group,ov_list))
+    
+def make_trans_rdm1(dspin, cibra, ciket, norb, nelec_bra,nelec_ket, po_list, group, ov_list):
+    """
+    One-particle transition density matrix between states
+    with different spins.
+
+    Args:
+        dspin : str 'aa' or 'bb' or 'ab' or 'ba'
+            the spin subscript of p, q operators
+        cibra : np.ndarray((n_det_alpha, n_det_beta))
+            ci vector for bra wavefunction
+        ciket : np.ndarray((n_det_alpha, n_det_beta))
+            ci vector for ket wavefunction
+        norb : int
+            number of orbitals
+            in this case, number of active orbitals
+        nelec_bra : (int, int)
+            numebr of alpha and beta electrons in bra state
+        nelec_ket : (int, int)
+            numebr of alpha and beta electrons in ket state
+        PO : (int, int)
+            possible occupation number pattern of active space
+        group: list or None
+            list : SF-NOCI with grouped bath
+            None : SF-NOCI        
+    Returns:
+        rdm1 : np.ndarray((norb, norb))
+            transition density matrix
+    """
+    nelabra, nelbbra = nelec_bra
+    nelaket, nelbket = nelec_ket
+    if dspin == 'ba':
+        cond = nelabra == nelaket - 1 and nelbbra == nelbket + 1
+    elif dspin == 'ab':
+        cond = nelabra == nelaket + 1 and nelbbra == nelbket - 1
+    elif dspin == 'aa':
+        cond = nelabra == nelaket and nelbbra == nelbket and nelabra > 0
+    else:
+        cond = nelabra == nelaket and nelbbra == nelbket and nelbbra > 0
+    if not cond:
+        return numpy.array(0)
+    nabra = fci.cistring.num_strings(norb, nelabra)
+    nbbra = fci.cistring.num_strings(norb, nelbbra)
+    naket = fci.cistring.num_strings(norb, nelaket)
+    nbket = fci.cistring.num_strings(norb, nelbket)
+    ketstringa = fci.cistring.make_strings(range(norb), nelaket)
+    ketstringb = fci.cistring.make_strings(range(norb), nelbket)
+    brastringa = fci.cistring.make_strings(range(norb), nelabra)
+    brastringb = fci.cistring.make_strings(range(norb), nelbbra)
+    cibra = cibra.reshape(nabra, nbbra)
+    ciket = ciket.reshape(naket, nbket)
+    rdm1 = numpy.zeros((norb,norb))
+    if cond:
+        if dspin == 'ba':
+            lidxa = fci.cistring.gen_des_str_index(range(norb),nelaket)
+            lidxb = fci.cistring.gen_cre_str_index(range(norb),nelbket)
+            for str0a in range(naket):
+                for str0b in range(nbket):
+                    for a, _, str1b, signb in lidxb[str0b]:
+                        for _ , i, str1a, signa in lidxa[str0a]:
+                            w_occa = sfnoci.str2occ(ketstringa[str0a],norb)
+                            w_occb = sfnoci.str2occ(ketstringb[str0b],norb)
+                            x_occa = sfnoci.str2occ(brastringa[str1a],norb)
+                            x_occb = sfnoci.str2occ(brastringb[str1b],norb)
+                            x_occ = x_occa + x_occb
+                            w_occ = w_occa + w_occb
+                            p1 = sfnoci.find_matching_rows(po_list,x_occ)
+                            p2 = sfnoci.find_matching_rows(po_list,w_occ)
+                            if group is not None:
+                                p1 = sfnoci.num_to_group(group,p1)
+                                p2 = sfnoci.num_to_group(group,p2)
+                            rdm1[a,i] += numpy.conjugate(cibra[str1a,str1b])*ciket[str0a,str0b]*ov_list[p1,p2]*signa *signb
+
+        elif dspin == 'ab':
+            lidxa = fci.cistring.gen_cre_str_index(range(norb),nelaket)
+            lidxb = fci.cistring.gen_des_str_index(range(norb),nelbket)
+            for str0a in range(naket):
+                for str0b in range(nbket):
+                    for a, _, str1a, signa in lidxa[str0a]:
+                        for _, i, str1b, signb in lidxb[str0b]:
+                            w_occa = sfnoci.str2occ(ketstringa[str0a],norb)
+                            w_occb = sfnoci.str2occ(ketstringb[str0b],norb)
+                            x_occa = sfnoci.str2occ(brastringa[str1a],norb)
+                            x_occb = sfnoci.str2occ(brastringb[str1b],norb)
+                            x_occ = x_occa + x_occb
+                            w_occ = w_occa + w_occb
+                            p1 = sfnoci.find_matching_rows(po_list,x_occ)
+                            p2 = sfnoci.find_matching_rows(po_list,w_occ)
+                            if group is not None:
+                                p1 = sfnoci.num_to_group(group,p1)
+                                p2 = sfnoci.num_to_group(group,p2)
+                            rdm1[a,i] += numpy.conjugate(cibra[str1a,str1b])*ciket[str0a,str0b]*ov_list[p1,p2]*signa *signb
+
+        elif dspin == 'aa':
+            lidxa = fci.cistring.gen_linkstr_index(range(norb),nelaket)
+            for str0a in range(naket):
+                for a, i, str1a, signa in lidxa[str0a]:
+                    for str0b in range(nbket):
+                        w_occa = sfnoci.str2occ(ketstringa[str0a],norb)
+                        w_occb = sfnoci.str2occ(ketstringb[str0b],norb)
+                        x_occa = sfnoci.str2occ(brastringa[str1a],norb)
+                        x_occ = x_occa + w_occb
+                        w_occ = w_occa + w_occb
+                        p1 = sfnoci.find_matching_rows(po_list,x_occ)
+                        p2 = sfnoci.find_matching_rows(po_list,w_occ)
+                        if group is not None:
+                            p1 = sfnoci.num_to_group(group,p1)
+                            p2 = sfnoci.num_to_group(group,p2)
+                        rdm1[a,i] += numpy.conjugate(cibra[str1a,str0b])*ciket[str0a,str0b]*ov_list[p1,p2]*signa
+        
+        elif dspin == 'bb':
+            lidxb = fci.cistring.gen_linkstr_index(range(norb),nelbket)
+            for str0b in range(nbket):
+                for a, i , str1b, signb in lidxb[str0b]:
+                    for str0a in range(naket):
+                        w_occa = sfnoci.str2occ(ketstringa[str0a],norb)
+                        w_occb = sfnoci.str2occ(ketstringb[str0b],norb)
+                        x_occb = sfnoci.str2occ(brastringb[str1b],norb)
+                        x_occ = w_occa + x_occb
+                        w_occ = w_occa + w_occb
+                        p1 = sfnoci.find_matching_rows(po_list,x_occ)
+                        p2 = sfnoci.find_matching_rows(po_list,w_occ)
+                        if group is not None:
+                            p1 = sfnoci.num_to_group(group,p1)
+                            p2 = sfnoci.num_to_group(group,p2)
+                        rdm1[a,i] += numpy.conjugate(cibra[str0a,str1b])*ciket[str0a,str0b]* ov_list[p1,p2]*signb
+        return rdm1
+    
+def kernel_siso_we(sfnoci, ci = None, po_list = None, group = None, ov_list = None, hsoao = None, amfi = True):
+    mol = sfnoci.mol
+    ncas = sfnoci.ncas
+    ncore = sfnoci.ncore
+    if ci is None: 
+        raise NotImplementedError
+    if po_list is None:
+        raise NotImplementedError
+    if ov_list is None:
+        raise NotImplementedError
+    
+    if hsoao is None:
+        print('\nGenerating Spin-Orbit Integrals:\n')
+        gsci = 0
+        for index, ici in enumerate(ci):
+            if ici[4] < ici[gsci][4]:
+                    gsci = index
+        if dmao is None: 
+            dmao = sfnoci.make_rdm1(ci[gsci][5], sfnoci.mo_coeff, nelecas= (ici[gsci][0],ici[gsci][1]))
+        hsoao = compute_hso_ao(mol, dmao, amfi = amfi) *2
+    hso = numpy.einsum('rij,ip,jq -> rpq',hsoao, sfnoci.mo_coeff[:,sfnoci.ncore:sfnoci.ncore + sfnoci.ncas],sfnoci.mo_coeff[:,sfnoci.ncore:sfnoci.ncore + sfnoci.ncas])
+    hso_pmz = numpy.zeros_like(hso)
+    hso_pmz[0] = (1j*hso[1] - hso[0])/2
+    hso_pmz[1] = (1j*hso[1] + hso[0])/2
+    hso_pmz[2] = hso[2]*numpy.sqrt(0.5)
+
+    # state interaction
+    #
+    su2cg = SU2CG()
+    #Spin multiplicity list
+    ms_dim = [ici[2]+1 for ici in ci]
+    print(ms_dim)
+    #index shift
+    idx_shift = [sum(ms_dim[:i]) for i in range(len(ms_dim))]
+    hdiag = numpy.array([ici[4] for ici in ci for ms in range(ici[2]+1)], dtype=complex)
+    hsiso = numpy.zeros((hdiag.shape[0], hdiag.shape[0]), dtype=complex)
+    thrds = 29.0  # cm-1
+    au2cm = nist.HARTREE2J / nist.PLANCK / nist.LIGHT_SPEED_SI * 1e-2
+    print("\nComplex SO-Hamiltonian matrix elements over spin components of spin-free eigenstates:")
+    print("(In cm-1. Print threshold:%10.3f cm-1)\n" % thrds)
+
+    for istate, ici in enumerate(ci):
+        for jstate, jci in enumerate(ci):
+            if jstate < istate:
+                continue
+            zero_me_ij = False
+            if abs(ici[2]-jci[2]) > 2:
+                zero_me_ij = True
+            elif ici[2] == jci[2] == 0:
+                zero_me_ij = True
+            elif abs(ici[3]-jci[3]) > 2:
+                zero_me_ij = True
+            elif jci[3] == ici[3] - 2:
+                CGcoeff = su2cg.clebsch_gordan(ici[2], 2, jci[2], ici[3], -2, jci[3])
+                if CGcoeff == 0:
+                    zero_me_ij = True
+                else:
+                    tp1 = make_trans(1, ici[-1], jci[-1],
+                            ncas, ici[:2], jci[:2],po_list, group, ov_list)
+                    ij_red_den = tp1 / CGcoeff
+            elif jci[3] == ici[3] + 2:
+                CGcoeff = su2cg.clebsch_gordan(ici[2], 2, jci[2], ici[3], 2, jci[3])
+                if CGcoeff == 0:
+                    zero_me_ij = True
+                else:
+                    tm1 = make_trans(-1, ici[-1], jci[-1],
+                            ncas, ici[:2], jci[:2],po_list, group, ov_list)
+                    ij_red_den = tm1 / CGcoeff
+            elif jci[3] == ici[3]:
+                CGcoeff = su2cg.clebsch_gordan(ici[2], 2, jci[2], ici[3], 0, jci[3])
+                if CGcoeff == 0:
+                    zero_me_ij = True
+                else:
+                    tze = make_trans(0, ici[-1], jci[-1],
+                            ncas, ici[:2], jci[:2],po_list, group, ov_list)
+                    ij_red_den = tze / CGcoeff
+            else:
+                zero_me_ij = True
+            for ii, i_ms2 in enumerate(range(-ici[2], ici[2] + 1, 2)):
+                for jj, j_ms2 in enumerate(range(-jci[2], jci[2] + 1, 2)):
+                    if zero_me_ij or abs(i_ms2 - j_ms2) > 2 :
+                        somat = 0
+                    elif j_ms2 == i_ms2 - 2:
+                        CGcoeff = su2cg.clebsch_gordan(ici[2], 2, jci[2], i_ms2, -2, j_ms2)
+                        somat = numpy.einsum('ij,ij->', ij_red_den, hso_pmz[0])*CGcoeff
+                    elif j_ms2 == i_ms2 + 2:
+                        CGcoeff = su2cg.clebsch_gordan(ici[2], 2, jci[2], i_ms2, 2, j_ms2)
+                        somat = numpy.einsum('ij,ij->', ij_red_den, hso_pmz[1])*CGcoeff
+                    elif j_ms2 == i_ms2:
+                        CGcoeff = su2cg.clebsch_gordan(ici[2], 2, jci[2], i_ms2, 0, j_ms2)
+                        somat = numpy.einsum('ij,ij->', ij_red_den, hso_pmz[2])*CGcoeff
+                    else:
+                        somat = 0
+                    hsiso[idx_shift[istate]+ii, idx_shift[jstate]+jj] = somat
+                    if istate != jstate:
+                        hsiso[idx_shift[jstate]+jj, idx_shift[istate]+ii] = numpy.conj(somat)
+                    somat *= au2cm
+                    if abs(somat) > thrds:
+                        print(('<%4d|H_SO|%4d> I1 = %4d (E1 = %15.8f) S1 = %4.1f MS1 = %4.1f '
+                                   + 'I2 = %4d (E2 = %15.8f) S2 = %4.1f MS2 = %4.1f '
+                                   + 'Re = %9.3f Im = %9.3f')
+                                  % (idx_shift[istate]+ii, idx_shift[jstate]+jj,
+                                     istate, ici[4], ici[2] / 2, i_ms2 / 2,
+                                     jstate, jci[4], jci[2] / 2, j_ms2 / 2,
+                                     somat.real, somat.imag))
+    hfull = hsiso + numpy.diag(hdiag)
+    heig, hvec = numpy.linalg.eigh(hfull)
+    for i in range(len(heig)):
+        sf_proj_vec = []
+        for ibra, mult in enumerate(ms_dim):
+            sf_proj_vec.append(numpy.linalg.norm(
+                hvec[idx_shift[ibra]:idx_shift[ibra] + mult, i]) ** 2)
+        iv = numpy.argmax(numpy.abs(sf_proj_vec))
+        print(('  State %4d Total energy: %15.8f | largest |proj_norm|**2 % 6.4f '
+                + 'from I = %4d E = %15.8f S = %4.1f')
+                % (i, heig[i], sf_proj_vec[iv], iv, ci[iv][4], ci[iv][2] / 2))
+    return heig, hvec
+
+def dipole_sdm_coreex_we(sfnoci,ci_ex,ci_gr,po_list, group, ov_list):
+    mol = sfnoci.mol
+    ncas = sfnoci.ncas
+    ncore = sfnoci.ncore
+    su2cg = SU2CG()
+    ms_dim_ex = [ci[2]+1 for ci in ci_ex]
+    idx_shift_ex = [sum(ms_dim_ex[:i]) for i in range(len(ms_dim_ex))]
+    ms_dim_gr = [ci[2]+1 for ci in ci_gr]
+    idx_shift_gr = [sum(ms_dim_gr[:i]) for i in range(len(ms_dim_gr))]
+    dip_ints = mol.intor_symmetric('int1e_r')
+    dipole_sdm = numpy.zeros((3,sum(ms_dim_ex),sum(ms_dim_gr)))
+     ##dipole calculation###
+    for istate, ici in enumerate(ci_ex):
+        for jstate, jci in enumerate(ci_gr):
+            if ici[2] == jci[2]:
+                if ici[3] != jci[3]: NotImplementedError
+                elif ici[3] == jci[3]:
+                    zero_me_ij = False   
+                    CGcoeff = su2cg.clebsch_gordan(ici[2], 0, jci[2], ici[3], 0, jci[3])
+                    if CGcoeff == 0:
+                        zero_me_ij = True
+                    else:
+                        rdm1a = make_trans_rdm1('aa', ici[-1], jci[-1], ncas, (ici[0], ici[1]), (jci[0],jci[1]),po_list, group, ov_list)
+                        rdm1b = make_trans_rdm1('bb', ici[-1], jci[-1], ncas, (ici[0], ici[1]), (jci[0],jci[1]),po_list, group, ov_list)
+                        trans_rdm = rdm1a + rdm1b
+                        mo_cas = sfnoci.mo_coeff[:,ncore:ncore + ncas]
+                        trans_rdm = lib.einsum('ij, ai, bj -> ab', trans_rdm, mo_cas, mo_cas)
+                        ij_red_den = trans_rdm / CGcoeff
+                for ii, i_ms2 in enumerate(range(-ici[2], ici[2] + 1, 2)):
+                    for jj, j_ms2 in enumerate(range(-jci[2], jci[2] + 1, 2)):
+                        if not zero_me_ij and i_ms2 == j_ms2:
+                            CGcoeff = su2cg.clebsch_gordan(ici[2], 0, jci[2], i_ms2, 0, j_ms2)
+                            dipole_sdm[:,idx_shift_ex[istate]+ii,idx_shift_gr[jstate]+jj] += lib.einsum('xij,ij ->x',dip_ints, ij_red_den) * CGcoeff
+    return dipole_sdm
+
+
+
---
+++ sfnocisiso/utils.py
@@ -0,0 +1,233 @@
+import numpy
+from pyscf import fci
+from pyscf.fci import cistring
+from pyscf.fci import fci_slow
+from pyscf.fci.direct_spin1 import spin_op
+from pyscf import lib
+from pyscf.sfnoci.sfnoci import str2occ, find_matching_rows, num_to_group
+
+def construct_hamiltonian_matrix(nelec,ncas,h1eff,eri,po_list,group, ov_list,ecore_list):
+    stringsa = cistring.make_strings(range(ncas),nelec[0])
+    stringsb = cistring.make_strings(range(ncas),nelec[1])
+    link_indexa = cistring.gen_linkstr_index(range(ncas),nelec[0])
+    link_indexb = cistring.gen_linkstr_index(range(ncas),nelec[1])
+    na = cistring.num_strings(ncas,nelec[0])
+    nb = cistring.num_strings(ncas,nelec[1])
+    idx_a = numpy.arange(na)
+    idx_b = numpy.arange(nb)
+    mat1 = numpy.zeros((na,nb,na,nb))
+    matTSc = numpy.zeros((na,nb,na,nb))
+    for str0a, taba in enumerate(link_indexa):
+        for pa, qa, str1a, signa in taba:
+            for str0b, tabb in enumerate(link_indexb):
+                for pb, qb, str1b, signb in tabb:
+                    w_occa = str2occ(stringsa[str0a],ncas)
+                    w_occb = str2occ(stringsb[str0b],ncas)
+                    x_occa = str2occ(stringsa[str1a],ncas)
+                    x_occb = str2occ(stringsb[str1b],ncas)
+                    x_state_occ = numpy.array(x_occa) + numpy.array(x_occb)
+                    w_state_occ = numpy.array(w_occa) + numpy.array(w_occb)
+                    p1=find_matching_rows(po_list,x_state_occ)[0]
+                    p2=find_matching_rows(po_list,w_state_occ)[0]
+                    if group is not None: 
+                       p1 = num_to_group(group,p1)
+                       p2 = num_to_group(group,p2) 
+                    if matTSc[str1a,str1b,str0a,str0b]==0:
+                        matTSc[str1a,str1b,str0a,str0b] += ov_list[p1,p2]    
+                    if pa==qa and pb ==qb:
+                        mat1[str1a,str1b,str0a,str0b] += (signa * h1eff[p1,p2,pa,qa]/nelec[1]  + signb * h1eff[p1,p2,pb,qb]/nelec[0])
+                    elif pa!=qa and pb == qb:
+                        mat1[str1a,str1b,str0a,str0b] += signa * h1eff[p1,p2,pa,qa]/nelec[1]
+                    elif pa==qa and pb !=qb:
+                        mat1[str1a,str1b,str0a,str0b] += signb * h1eff[p1,p2,pb,qb]/nelec[0]
+                    elif pa!=qa and pb !=qb:
+                        mat1[str1a,str1b,str0a,str0b] += 0
+                    #mat1[str1a,idx_b,str0a,idx_b] += signa * h1c[g1,g2,pa,qa]
+                    #mat1[idx_a,str1b,idx_a,str0b] += signb * h1c[g1,g2,pb,qb]
+
+    #mat1 = mat1.reshape(na*nb,na*nb)
+    h2 = fci_slow.absorb_h1e(h1eff[0,0]*0, eri, ncas, nelec)
+    t1 = numpy.zeros((ncas,ncas,na,nb,na,nb))
+    for str0, tab in enumerate(link_indexa):
+        for a, i, str1, sign in tab:
+            # alpha spin
+            t1[a,i,str1,idx_b,str0,idx_b] += sign
+    for str0, tab in enumerate(link_indexb):
+        for a, i, str1, sign in tab:
+            # beta spin
+            t1[a,i,idx_a,str1,idx_a,str0] += sign
+    t1 = lib.einsum('psqr,qrABab->psABab', h2, t1)
+    mat2 = numpy.zeros((na,nb,na,nb))
+    for str0, tab in enumerate(link_indexa):
+        for a, i, str1, sign in tab:
+            # alpha spin
+            mat2[str1] += sign * t1[a,i,str0]
+    for str0, tab in enumerate(link_indexb):
+        for a, i, str1, sign in tab:
+           # beta spin
+            mat2[:,str1] += sign * t1[a,i,:,str0]
+    #mat2 = mat2.reshape(na*nb,na*nb)
+    ham = (mat1+0.5*mat2)*matTSc
+    ham = ham.reshape(na*nb,na*nb)
+    K = numpy.zeros((na,nb))
+    for i in range(0,na):
+        for j in range(0,nb):
+            x_occa = str2occ(stringsa[i],ncas)
+            x_occb = str2occ(stringsb[j],ncas)        
+            x_state_occ = numpy.array(x_occa) + numpy.array(x_occb)
+            p1=find_matching_rows(po_list,x_state_occ)[0]
+            if group is not None: 
+                p1 = num_to_group(group,p1) 
+            K[i,j] = ecore_list[p1]
+    K = K.reshape(-1)
+    K = numpy.diag(K)
+    # print("mat1")
+    # print(mat1.reshape(na*nb,na*nb)) 
+    # print("mat2")
+    # print(0.5*mat2.reshape(na*nb,na*nb))
+    # print("K")
+    # print(K)
+    hamiltonian = ham + K 
+    return hamiltonian
+
+def make_non_excited_string(nras1, nras2, neleras2):
+    ras1 = range(0, nras1)
+    ras2 = range(nras1, nras1 + nras2)
+    ras1string0 = cistring.make_strings(ras1, nras1)
+    ras2string0 = cistring.make_strings(ras2, neleras2)
+    rasstring0 = numpy.add.outer(ras1string0,ras2string0).flatten()
+    return numpy.array(rasstring0, order = 'C', dtype = numpy.int64)
+
+def contract_spin_up(civec_strs, norb, nelec, s2, ms2):
+    neleca, nelecb = fci.addons._unpack_nelec(nelec)
+    na = cistring.num_strings(norb, neleca)
+    nb = cistring.num_strings(norb, nelecb)
+    fcivec = fcivec.reshape(na,nb)
+
+    def gen_map(fstr_index, nelec, des=True):
+        a_index = fstr_index(range(norb), nelec)
+        amap = numpy.zeros((a_index.shape[0],norb,2), dtype=numpy.int32)
+        if des:
+            for k, tab in enumerate(a_index):
+                amap[k,tab[:,1]] = tab[:,2:]
+        else:
+            for k, tab in enumerate(a_index):
+                amap[k,tab[:,0]] = tab[:,2:]
+        return amap
+
+    
+    if neleca > 0:
+        ades = gen_map(cistring.gen_des_linkstr, neleca)
+    else:
+        ades = None
+
+    if nelecb > 0:
+        bdes = gen_map(cistring.gen_des_linkstr, nelecb)
+    else:
+        bdes = None
+
+    if neleca < norb:
+        acre = gen_map(cistring.gen_cre_linkstr, neleca, False)
+    else:
+        acre = None
+
+    if nelecb < norb:
+        bcre = gen_map(cistring.gen_cre_linkstr, nelecb, False)
+    else:
+        bcre = None
+    def trans(ci_coeff, aindex, bindex):
+        if aindex is None or bindex is None:
+            return None
+
+        ma = len(aindex)
+        mb = len(bindex)
+        t1 = numpy.zeros((ma,mb))
+        for i in range(norb):
+            signa = aindex[:,i,1]
+            signb = bindex[:,i,1]
+            maska = numpy.where(signa!=0)[0]
+            maskb = numpy.where(signb!=0)[0]
+            addra = aindex[maska,i,0]
+            addrb = bindex[maskb,i,0]
+            citmp = lib.take_2d(ci_coeff, addra, addrb)
+            citmp *= signa[maska].reshape(-1,1)
+            citmp *= signb[maskb]
+            #: t1[addra.reshape(-1,1),addrb] += citmp
+            lib.takebak_2d(t1, citmp, maska, maskb)
+        return t1
+        
+    ci1 = trans(fcivec, acre, bdes)
+    return ci1 / (numpy.sqrt(s2 / 2 * (s2 / 2 + 1) - ms2 / 2 * (ms2 / 2 + 1)))
+
+def ci_gr_coreex_by_diag(sfnoci, nras1, nras2, nreleras2, num_gr, num_ex, h1eff,eri,po_list,group, ov_list,ecore_list):
+    nelecas = sfnoci.nelecas
+    neleca, nelecb = nreleras2
+    ncas = nras1 + nras2
+    ms2 = neleca - nelecb
+
+    rasstring0a = make_non_excited_string(nras1, nras2, neleca)
+    rasstring0b = make_non_excited_string(nras1, nras2, nelecb)
+    hamiltonian = construct_hamiltonian_matrix((nras1 + neleca, nras1 + nelecb), ncas, h1eff,eri,po_list,group, ov_list,ecore_list)
+
+    n0a = len(rasstring0a)
+    n0b = len(rasstring0b)
+    e, ci = numpy.linalg.eigh(hamiltonian)
+    index = numpy.argsort(e)
+    e = e[index]
+    ci = ci[:,index]
+
+    ci_gr = []
+    ci_ex = []
+    for i in range(num_gr):
+        cci = ci[:,i]
+        ss, multi = spin_op.spin_square0(ci[:,i], ncas, (nras1 + neleca, nras1 + nelecb))
+        if round(multi) == 1:
+            ci_gr.append((nras1 + neleca, nras1 + nelecb, round(multi)-1, ms2, e[i], cci))
+        elif round(multi) == 3:
+                    #uci = get_spin_up_state(cci, nras1 + nras2, (nras1 + neleca, nras1 + nelecb))
+            uci = contract_spin_up(cci, nras1 + nras2, (nras1 + neleca, nras1 + nelecb), round(multi)-1 , ms2)
+            ci_gr.append((nras1 + neleca + 1, nras1 + nelecb - 1, round(multi)-1, ms2 + 2, e[i], uci))
+                    #dci = get_spin_down_state(cci, nras1 + nras2, (nras1 + neleca, nras1 + nelecb))
+                    #self.ci.append((nras1+ neleca - 1, nras1 + nelecb +1, round(multi)-1, ms2 - 2, e[i], dci))
+        elif round(multi) == 5:
+            cci = contract_spin_up(cci, nras1 + nras2, (nras1 + neleca, nras1 + nelecb), round(multi)-1, ms2)
+            cci = contract_spin_up(cci, nras1 + nras2, (nras1 + neleca + 1, nras1 + nelecb - 1), round(multi)-1, ms2 + 2)
+            ci_gr.append((nras1 + neleca + 2, nras1 + nelecb - 2, round(multi)-1, ms2 + 4, e[i], cci))
+
+        elif round(multi) == 2:
+            ci_gr.append((nras1 + neleca, nras1 + nelecb, round(multi)-1, ms2, e[i], cci))
+        elif round(multi) == 4:
+            uci = contract_spin_up(cci, nras1 + nras2, (nras1 + neleca, nras1 + nelecb), round(multi)-1 , ms2)
+            ci_gr.append((nras1 + neleca + 1, nras1 + nelecb - 1, round(multi)-1, ms2 + 2, e[i], uci))
+        elif round(multi) == 6:
+            cci = contract_spin_up(cci, nras1 + nras2, (nras1 + neleca, nras1 + nelecb), round(multi)-1, ms2)
+            cci = contract_spin_up(cci, nras1 + nras2, (nras1 + neleca + 1, nras1 + nelecb - 1), round(multi)-1, ms2 + 2)
+            ci_gr.append((nras1 + neleca + 2, nras1 + nelecb - 2, round(multi)-1, ms2 + 4, e[i], cci))
+    e = e[n0a*n0b:]
+    ci = ci[:,n0a*n0b:]
+    for i in range(num_ex):
+        cci = ci[:,i]
+        ss, multi = spin_op.spin_square0(ci[:,i], ncas, (nras1 + neleca, nras1 + nelecb))
+        if round(multi) == 1:
+            ci_ex.append((nras1 + neleca, nras1 + nelecb, round(multi)-1, ms2, e[i], cci))
+        elif round(multi) == 3:
+                    #uci = get_spin_up_state(cci, nras1 + nras2, (nras1 + neleca, nras1 + nelecb))
+            uci = contract_spin_up(cci, nras1 + nras2, (nras1 + neleca, nras1 + nelecb), round(multi)-1 , ms2)
+            ci_ex.append((nras1 + neleca + 1, nras1 + nelecb - 1, round(multi)-1, ms2 + 2, e[i], uci))
+                    #dci = get_spin_down_state(cci, nras1 + nras2, (nras1 + neleca, nras1 + nelecb))
+                    #self.ci.append((nras1+ neleca - 1, nras1 + nelecb +1, round(multi)-1, ms2 - 2, e[i], dci))
+        elif round(multi) == 5:
+            cci = contract_spin_up(cci, nras1 + nras2, (nras1 + neleca, nras1 + nelecb), round(multi)-1, ms2)
+            cci = contract_spin_up(cci, nras1 + nras2, (nras1 + neleca + 1, nras1 + nelecb - 1), round(multi)-1, ms2 + 2)
+            ci_ex.append((nras1 + neleca + 2, nras1 + nelecb - 2, round(multi)-1, ms2 + 4, e[i], cci))
+
+        elif round(multi) == 2:
+            ci_ex.append((nras1 + neleca, nras1 + nelecb, round(multi)-1, ms2, e[i], cci))
+        elif round(multi) == 4:
+            uci = contract_spin_up(cci, nras1 + nras2, (nras1 + neleca, nras1 + nelecb), round(multi)-1 , ms2)
+            ci_ex.append((nras1 + neleca + 1, nras1 + nelecb - 1, round(multi)-1, ms2 + 2, e[i], uci))
+        elif round(multi) == 6:
+            cci = contract_spin_up(cci, nras1 + nras2, (nras1 + neleca, nras1 + nelecb), round(multi)-1, ms2)
+            cci = contract_spin_up(cci, nras1 + nras2, (nras1 + neleca + 1, nras1 + nelecb - 1), round(multi)-1, ms2 + 2)
+            ci_ex.append((nras1 + neleca + 2, nras1 + nelecb - 2, round(multi)-1, ms2 + 4, e[i], cci))
+    return ci_gr, ci_ex
